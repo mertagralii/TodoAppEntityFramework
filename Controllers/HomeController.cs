fb395731aa1cf050ff806d70e1c1167e1dcd14ed
Mert Ağralı <my_mert07hotmail.com>
2025-02-18 20:16:44 +0300

    EntityFramework Listeleme Yöntemi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,18 +16,10 @@ namespace TodoAppEntityFramework.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var todolist = _context.Todos.ToList(); // EntityFramework Listeleme yöntemi
+            return View(todolist);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
