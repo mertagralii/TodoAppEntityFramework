1774b52e45bc64937aa019ec870bbdf8d5471022
Mert Ağralı <my_mert07hotmail.com>
2025-02-18 19:31:34 +0300

    Veritabanı modellerimizi oluşturduk.

---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoAppEntityFramework.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        public string JobName { get; set; }
+
+        public bool IsApproved { get; set; }
+    }
+}
