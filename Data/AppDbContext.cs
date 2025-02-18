f194023e0f57ceee25ff4a94117b3269f2934a82
Mert Ağralı <my_mert07hotmail.com>
2025-02-18 19:31:17 +0300

    Data adında bir klasör oluşturdum. AppDbContext adında klass oluşturup veritabanı bloglarımızı bağladık ve gönderdik.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAppEntityFramework.Models;
+
+namespace TodoAppEntityFramework.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
